f497a331526aa0724adf78bb6733e1a442993f65
gs <ghostson1120@outlook.com>
2022-10-07 17:08:54 +0300

    initial

---
+++ src/views/BookDetails.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="books">
+        <div class="card">
+            <div class="card-desc">
+                <img :src="`http://localhost:1337${image}`" />
+                <div>
+                    <h3>{{ bookById.Name }}</h3>
+                    <h5>By: {{ bookById.Author }}</h5>
+                    <h5>Published at: {{ bookById.publishedAt }}</h5>
+                </div>
+                <p>{{ bookById.Description }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        name: "BookDetails",
+        data() {
+            return {
+                image: [],
+                bookById: [],
+            };
+        },
+        async mounted() {
+            const id = this.$route.params.id;
+            await axios
+                .get(`http://localhost:1337/api/books/${id}?populate=*`)
+                .then((response) => {
+                this.bookById = response.data.data.attributes;
+                this.image = this.bookById.Image.data.attributes.url;
+                });
+        },
+    };
+</script>
+
+<style scoped>
+    .card {
+        max-width: 80vw;
+        margin: auto;
+    }
+    .card-title {
+        display: flex;
+        justify-content: space-around;
+    }
+    img {
+        width: 20vw;
+        height: 20vw;
+    }
+    h3 {
+        font-size: 20px;
+        color: #000;
+    }
+    h5 {
+        font-size: 15px;
+        font-style: italic;
+        color: #716c69;
+        margin: 5px !important;
+    }
+    p {
+        font-size: 19px;
+        color: #000;
+        width: 50vw;
+        padding-top: 20px;
+        margin: auto;
+        text-align: justify;
+    }
+    button {
+        padding: 15px;
+        background: blue;
+        border: none;
+        color: #fff;
+        width: 100%;
+    }
+    .card-desc {
+        margin: 0px 50px;
+    }
+    .title, .author {
+        padding: 5px;
+    }
+</style>
---
+++ src/views/BookView.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="row">
+      <div class="cols" v-for="book in getBookStore" :key="book.key">
+        <router-link :to="{ name: 'BookDetails', params: { id: book.id } }">
+          <img
+            :src="`http://localhost:1337${book.attributes.Image.data.attributes.url}`"
+          />
+          <h3>{{ book.attributes.Name }}</h3>
+          <h5>
+            By :
+            {{ book.attributes.Author }}
+          </h5>
+          <h5>
+            Published at: <br />
+            {{ book.attributes.publishedAt }}
+          </h5>
+        </router-link>
+      </div>
+    </div>
+  </template>
+  <script>
+  import { mapGetters, mapActions } from "vuex";
+  export default {
+    name: "BookStore",
+    data() {
+      return {
+        bookStore: [],
+      };
+    },
+    mounted() {
+      this.getAllBooks();
+    },
+    computed: {
+      ...mapGetters(["getBookStore"]),
+    },
+    methods: {
+      ...mapActions(["getAllBooks"]),
+    },
+  };
+  </script>
+<style scoped>
+  .row {
+    margin: 0px 10px;
+    display: flex;
+    text-align: justify;
+  }
+  .cols {
+    margin: 10px;
+    width: 15vw;
+    box-shadow: 4px 4px 4px #ae9f9f;
+  }
+  img {
+    width: 100%;
+    height: 150px;
+  }
+  a {
+    text-decoration: none;
+    color: #201c1c;
+  }
+  p {
+    font-size: 15px;
+    text-align: left;
+  }
+  h3,
+  h5 {
+    margin: 10px 10px;
+  }
+</style>
